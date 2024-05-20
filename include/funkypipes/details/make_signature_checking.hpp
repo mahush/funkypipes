498f24b8d5e70b3aede27a92fd4904b7966d4529
mahush <info@mahush.de>
2024-05-20 21:12:37 +0200

    add multiple arguments support for signature checking

--- include/funkypipes/details/make_signature_checking.hpp
+++ include/funkypipes/details/make_signature_checking.hpp
@@ -16,15 +16,15 @@ namespace funkypipes::details {
 
 // Helper template struct encapsulating static_asserts for clearer compiler errors. Its template parameters will be
 // listed when instantiation fails due to static_asserts.
-template <typename TFn, typename TArg>
+template <typename TFn, typename... TArgs>
 struct AssertInvocability {
-  static_assert(std::is_invocable_v<TFn, TArg>,
-                "Callable TFn does not support arguments of type TArg. Look for AssertInvocability in the compiler's "
+  static_assert(std::is_invocable_v<TFn, TArgs...>,
+                "Callable TFn does not support arguments of type TArgs. Look for AssertInvocability in the compiler's "
                 "error output to figure out the relevant types TFn and TArg.");
-  using ResultType = typename std::invoke_result<TFn, TArg>::type;
+  using ResultType = typename std::invoke_result<TFn, TArgs...>::type;
   static_assert(not std::is_void_v<ResultType>,
                 "Result of TFn may not be void. Look for AssertInvocability in the compiler's error output to figure "
-                "out the relevant types TFn and TArg.");
+                "out the relevant types TFn and TArgs.");
 };
 
 // Helper template function that adds static_asserts to the given function to check the signature of the given function.
@@ -33,9 +33,11 @@ struct AssertInvocability {
 //  - does not return void
 template <typename TFn>
 auto makeSignatureChecking(TFn&& fn) {
-  return [fn = std::forward<TFn>(fn)](auto&& arg) mutable {
-    AssertInvocability<TFn, decltype(arg)>{};
-    return fn(std::forward<decltype(arg)>(arg));
+  return [fn = std::forward<TFn>(fn)](auto&&... args) mutable {
+    (void)AssertInvocability<TFn, decltype(args)...>{};  // Note: An unused instance is created to ensure
+                                                         // that this template is evaluated before the
+                                                         // next line.
+    return fn(std::forward<decltype(args)>(args)...);
   };
 }
 
