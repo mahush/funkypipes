40b34ba17a14fc2a406611f5213a88ad2207aa44
mahush <info@mahush.de>
2024-12-22 20:41:55 +0100

    add ComplementIndexSequence

--- include/funkypipes/details/tuple/index_sequence.hpp
+++ include/funkypipes/details/tuple/index_sequence.hpp
@@ -31,6 +31,38 @@ constexpr auto indexSequenceCat(TIndexSequences...) {
   return (TIndexSequences{} + ...);
 }
 
+//
+// ComplementIndexSequence
+//
+
+namespace impl {
+template <std::size_t SequenceSize, std::size_t... IdxsToBeComplemented>
+struct ComplementIndexSequenceImpl {
+  template <std::size_t Idx>
+  static constexpr bool is_complement = ((Idx != IdxsToBeComplemented) && ...);
+
+  template <std::size_t Idx>
+  using index_if_complement = std::conditional_t<is_complement<Idx>, std::index_sequence<Idx>, std::index_sequence<> >;
+
+  template <std::size_t... Idxs>
+  static constexpr auto makeComplementIndexSequence(std::index_sequence<Idxs...>) {
+    static_assert(SequenceSize >= sizeof...(IdxsToBeComplemented), "Sequence size smaller then given indices");
+    if constexpr (SequenceSize == 0) {
+      return std::index_sequence<>{};
+    } else {
+      return indexSequenceCat(index_if_complement<Idxs>{}...);
+    }
+  }
+
+  using type = decltype(makeComplementIndexSequence(std::make_index_sequence<SequenceSize>{}));
+};
+}  // namespace impl
+
+// A template to generates a complementary index sequence for the given size and indices
+template <std::size_t OverallIndexCount, std::size_t... IndicesToBeComplemented>
+using ComplementIndexSequence =
+    typename impl::ComplementIndexSequenceImpl<OverallIndexCount, IndicesToBeComplemented...>::type;
+
 }  // namespace funkypipes::details
 
 #endif  // FUNKYPIPES_DETAILS_TUPLE_INDEX_SEQUENCE_HPP
--- tests/details/tuple/test_index_sequence.cpp
+++ tests/details/tuple/test_index_sequence.cpp
@@ -9,6 +9,7 @@
 #include "funkypipes/details/tuple/index_sequence.hpp"
 
 using funkypipes::details::indexSequenceCat;
+using funkypipes::details::ComplementIndexSequence;
 
 static_assert(std::is_same_v<decltype(indexSequenceCat(std::index_sequence<0>{})), std::index_sequence<0>>);
 static_assert(std::is_same_v<decltype(indexSequenceCat(std::index_sequence<0>{}, std::index_sequence<>{})),
@@ -17,3 +18,8 @@ static_assert(std::is_same_v<decltype(indexSequenceCat(std::index_sequence<0, 1>
                              std::index_sequence<0, 1, 3, 4>>);
 static_assert(std::is_same_v<decltype(indexSequenceCat(std::index_sequence<0, 1>{}, std::index_sequence<0, 1>{})),
                              std::index_sequence<0, 1, 0, 1>>);
+
+static_assert(std::is_same_v<ComplementIndexSequence<4, 1, 3>, std::index_sequence<0, 2>>);
+static_assert(std::is_same_v<ComplementIndexSequence<2>, std::index_sequence<0, 1>>);
+static_assert(std::is_same_v<ComplementIndexSequence<2, 0, 1>, std::index_sequence<>>);
+static_assert(std::is_same_v<ComplementIndexSequence<0>, std::index_sequence<>>);
